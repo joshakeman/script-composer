fca3f4af023020238da1a3a5a74819a20e2e2dba
Josh Akeman <joshakeman@gmail.com>
2021-10-25 19:47:30 -0400

    added tabs

--- src/components/ScriptContainer.tsx
+++ src/components/ScriptContainer.tsx
@@ -5,14 +5,38 @@ import { Line } from './Constants'
 import update from 'immutability-helper'
 import { DayContainer } from './DayContainer'
 import Paper from '@mui/material/Paper';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
-interface Day {
+interface ScriptState {
+    days: Array<Object>
 }
 export default function ScriptContainer() {
-    const [days, setDays] = useState([{},{},{},{},{}])
+    const [days, setDays] = useState<ScriptState["days"]>([])
 
+    const addDay = () => {
+        setDays(prevState => [...prevState,{}])
+    }
+    
+    const [value, setValue] = React.useState(1);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+      setValue(newValue);
+    };
     return (
             <div>
+                <Button variant="contained" onClick={addDay}>Add Day</Button>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    textColor="secondary"
+                    indicatorColor="secondary"
+                    aria-label="secondary tabs example"
+                >
+                    {
+                        days.map((day, index) => <Tab value={index+1} label={index+1} />)
+                    }
+                </Tabs>
                 {days.map((day, index) => <DayContainer index={index}/>)}
             </div>
     )
