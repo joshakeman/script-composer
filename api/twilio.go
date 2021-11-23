c17e84d7d239e38ab469c052d174c7835ebbeb05
Josh Akeman <joshakeman@gmail.com>
2021-11-22 19:34:10 -0500

    created provision twilio number func

--- api/twilio.go
+++ api/twilio.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
 type TwilioClient struct {
@@ -21,6 +22,29 @@ func NewTwilioClient(c TwilioCredentials) *TwilioClient {
 func (tw *TwilioClient) ListNumbers() (TwilioListNumbersRes, int, error) {
 	url := fmt.Sprintf("https://api.twilio.com/2010-04-01/Accounts/%s/AvailablePhoneNumbers/US/Local.json", tw.Creds.AccountSid)
 	req, _ := http.NewRequest(http.MethodGet, url, nil)
+	req.SetBasicAuth(tw.Creds.AccountSid, tw.Creds.AuthToken)
+	req.Header.Add("Accept", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return TwilioListNumbersRes{}, 0, err
+	}
+	defer res.Body.Close()
+
+	var resObj TwilioListNumbersRes
+	json.NewDecoder(res.Body).Decode(&resObj)
+
+	p, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(p))
+
+	return resObj, res.StatusCode, nil
+}
+
+func (tw *TwilioClient) ProvisionNumber(number string) (TwilioListNumbersRes, int, error) {
+	params := url.Values{}
+	params.Add("PhoneNumber", number)
+	callUrl := fmt.Sprintf("https://api.twilio.com/2010-04-01/Accounts/%s/ncomingPhoneNumbers.json?%s", tw.Creds.AccountSid, params.Encode())
+	req, _ := http.NewRequest(http.MethodGet, callUrl, nil)
 	req.SetBasicAuth(tw.Creds.AccountSid, tw.Creds.AuthToken)
 	req.Header.Add("Accept", "application/json")
 
